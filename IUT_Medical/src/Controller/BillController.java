3f32f4400030e4c41ff56b2bfe94958df6e66606
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2024-05-06 21:24:36 +0600

    billcontroller created

---
+++ IUT_Medical/src/Controller/BillController.java
@@ -0,0 +1,52 @@
+package Controller;
+import java.util.Scanner;
+import java.util.List;
+
+public class BillController extends Controller{
+    public void create() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the referral ID:");
+        int referralID = sc.nextInt();
+        System.out.println("Enter the amount:");
+        int amount = sc.nextInt();
+        Model.Bill bill = new Model.Bill(referralID, amount);
+        bill.save();
+    }
+
+    public void details(){
+
+    }
+
+    public void update(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Bill ID:");
+        int billID = sc.nextInt();
+
+        Model.Bill bill = new Model.Bill(billID);
+
+        System.out.println("Enter the new referral ID:");
+        int referralID = sc.nextInt();
+
+        Model.Referral referral = new Model.Referral(referralID);
+
+        System.out.println("Enter the new amount:");
+        int amount = sc.nextInt();
+
+        bill.referral = referral;
+        bill.Amount = amount;
+        bill.update();
+
+        System.out.println("--- Bill updated successfully ---");
+    }
+
+    public void delete(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Bill ID:");
+        int billID = sc.nextInt();
+
+        Model.Bill bill = new Model.Bill(billID);
+        bill.delete();
+
+        System.out.println("--- Bill deleted successfully ---");
+    }
+}
